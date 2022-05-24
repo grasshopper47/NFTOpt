483e97c5246c918404f06d9575cea8e226209695
grasshoper47 <A@A.a>
2022-05-24 15:22:12 +0200

    update publicUptioRequest tests

--- test/publishOptionRequest.ts
+++ test/publishOptionRequest.ts
@@ -1,4 +1,4 @@
-import {expect} from "chai";
+import { expect } from "chai";
 import {
     address0,
     buyer,
@@ -20,21 +20,21 @@ describe("NFTOpt Tests", function () {
 
     describe("publishOptionRequest", function () {
         it("should fail when called with address(0) as NFT Contract Address", async function () {
-            await expect(NFTOptCTR.connect(buyer).publishOptionRequest(address0, 0, 0, 0, 0)).to.be.revertedWith(
-                "NFT contract must be a valid address"
-            );
+            await expect(NFTOptCTR.connect(buyer)
+                .publishOptionRequest(address0, 0, 0, 0, 0))
+                .to.be.revertedWith("NFT contract must be a valid address");
         });
 
         it("should fail when called with 0 as NFT Token ID", async function () {
-            await expect(NFTOptCTR.connect(buyer).publishOptionRequest(buyer.address, 0, 0, 0, 0)).to.be.revertedWith(
-                "NFT token ID must be > 0"
-            );
+            await expect(NFTOptCTR.connect(buyer)
+                .publishOptionRequest(buyer.address, 0, 0, 0, 0))
+                .to.be.revertedWith("NFT token ID must be > 0");
         });
 
         it("should fail when called with an invalid (non ERC-721 compliant) NFT Contract", async function () {
-            await expect(NFTOptCTR.connect(buyer).publishOptionRequest(buyer.address, 1, 0, 0, 0)).to.be.revertedWith(
-                "Provided NFT contract address must implement ERC-721 interface"
-            );
+            await expect(NFTOptCTR.connect(buyer)
+                .publishOptionRequest(buyer.address, 1, 0, 0, 0))
+                .to.be.revertedWith("Provided NFT contract address must implement ERC-721 interface");
         });
 
         it("should fail when NFT Token ID is under different ownership than the caller's", async function () {
@@ -46,37 +46,43 @@ describe("NFTOpt Tests", function () {
 
             expect(owner).to.be.equal(seller.address);
 
-            await expect(NFTOptCTR.connect(buyer).publishOptionRequest(NFTDummyCTR.address, _nftID, 0, 0, 0)).to.be.revertedWith(
-                "NOT_NFT_OWNER"
-            );
+            await expect(NFTOptCTR.connect(buyer)
+                .publishOptionRequest(NFTDummyCTR.address, _nftID, 0, 0, 0))
+                .to.be.revertedWith("NOT_NFT_OWNER");
         });
 
         it("should fail when called without a premium (transaction value)", async function () {
-            await expect(NFTOptCTR.connect(buyer).publishOptionRequest(NFTDummyCTR.address, 1, 0, 0, 0)).to.be.revertedWith(
-                "Premium must be > 0"
-            );
+            await expect(NFTOptCTR.connect(buyer)
+                .publishOptionRequest(NFTDummyCTR.address, 1, 0, 0, 0))
+                .to.be.revertedWith("Premium must be > 0");
         });
 
         it("should fail when called with 0 as Strike Price", async function () {
-            await expect(NFTOptCTR.connect(buyer).publishOptionRequest(NFTDummyCTR.address, 1, 0, 0, 0, {value: 1})).to.be.revertedWith(
-                "Strike price must be > 0"
-            );
+            await expect(NFTOptCTR.connect(buyer)
+                .publishOptionRequest(NFTDummyCTR.address, 1, 0, 0, 0, { value: 1 }))
+                .to.be.revertedWith("Strike price must be > 0");
         });
 
         it("should fail when called with 0 as Interval", async function () {
-            await expect(NFTOptCTR.connect(buyer).publishOptionRequest(NFTDummyCTR.address, 1, 1, 0, 0, {value: 1})).to.be.revertedWith(
-                "Expiration interval must be > 0"
-            );
+            await expect(NFTOptCTR.connect(buyer)
+                .publishOptionRequest(NFTDummyCTR.address, 1, 1, 0, 0, { value: 1 }))
+                .to.be.revertedWith("Expiration interval must be > 0");
         });
 
         it("should succeed when called with valid values", async function () {
-            expect(await NFTOptCTR.getBalance()).to.equal(0);
-            expect(await NFTOptCTR.optionID()).to.equal(0);
+            const balance0 = await NFTOptCTR.getBalance();
+            const optionID0 = await NFTOptCTR.optionID();
+
+            expect(balance0).to.equal(0);
+            expect(optionID0).to.equal(0);
 
             await publishDummyOptionRequest();
 
-            expect(await NFTOptCTR.getBalance()).to.equal(dummyOptionRequest.premium);
-            expect(await NFTOptCTR.optionID()).to.equal(1);
+            const balance1 = await NFTOptCTR.getBalance();
+            const optionID1 = await NFTOptCTR.optionID();
+
+            expect(balance1).to.equal(dummyOptionRequest.premium);
+            expect(optionID1).to.equal(1);
 
             // Check that details of the option data match those sent
             const option = await NFTOptCTR.options(1);
@@ -94,7 +100,9 @@ describe("NFTOpt Tests", function () {
         });
 
         it("should emit NewRequest event when succeeded", async function () {
-            await expect(publishDummyOptionRequest()).to.emit(NFTOptCTR, "NewRequest").withArgs(buyer.address, 1);
+            await expect(publishDummyOptionRequest())
+                .to.emit(NFTOptCTR, "NewRequest")
+                .withArgs(buyer.address, 1);
         });
     });
 });
