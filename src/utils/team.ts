06f1c29894f7d2ed5bf7e5546c086473f6d3c7e3
grasshoper47 <A@A.a>
2022-05-28 19:54:23 +0200

    Add photos for all team members

--- src/utils/team.ts
+++ src/utils/team.ts
@@ -10,10 +10,59 @@ export type Person = {
 };
 
 export const team: Person[] = [
+
+    // TODO: cleanup dummy data after everybody commits their info
     {
-        // TODO: cleanup dummy data after everybody commits their info
-        name: "Your name here",
-        social: [{name: "Your social ref here", url: "Your social url here"}],
-        image: "LW-433-400x422.jpg",
+        name: "Shabab A",
+        social: [{ name: "Your social ref here", url: "Your social url here" }],
+        image: "placeholder.jpg",
+    },
+
+    {
+        name: "Stefana M",
+        social: [{ name: "Your social ref here", url: "Your social url here" }],
+        image: "stef.jpg",
+    },
+
+    {
+        name: "Luis I",
+        social: [{ name: "Your social ref here", url: "Your social url here" }],
+        image: "luis.jpg",
+    },
+
+    {
+        name: "Greg v. D",
+        social: [{ name: "Your social ref here", url: "Your social url here" }],
+        image: "greg.jpg",
+    },
+
+    {
+        name: "42",
+        social: [],
+        // image: ""
+    },
+
+    {
+        name: "Peter A",
+        social: [{ name: "Your social ref here", url: "Your social url here" }],
+        image: "peter.jpg",
+    },
+
+    {
+        name: "Coach - Rahul",
+        social: [{ name: "Your social ref here", url: "Your social url here" }],
+        image: "shubham.jpg",
+    },
+
+    {
+        name: "Master of Ceremonies - Preethi",
+        social: [{ name: "Your social ref here", url: "Your social url here" }],
+        image: "preethi.jpg",
+    },
+
+    {
+        name: "Coach - Shubam",
+        social: [{ name: "Your social ref here", url: "Your social url here" }],
+        image: "rahul.jpg",
     },
 ];
