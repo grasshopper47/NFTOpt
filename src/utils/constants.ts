1ccb563e50c463bf4fe987fc90b3bbc9f0d0977e
grasshoper47 <A@A.a>
2022-05-28 22:33:18 +0200

    add MAX_MINTABLE_TOKENS

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,3 +11,4 @@ export const floatNumberRegex = /^([0-9]*[.])?[0-9]*$/;
 export const networkName: string = networks[networkId];
 export const SECONDS_IN_A_DAY = 86400;
 export const TOAST_DURATION = 4000;
+export const MAX_MINTABLE_TOKENS = 5;
