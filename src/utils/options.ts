1206f0e91015d1df3279ea8f3150e938f91cd450
grasshoper47 <A@A.a>
2022-05-28 22:34:04 +0200

    refactor formatting and some names

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -1,8 +1,9 @@
-import {BigNumber} from "ethers";
-import {NFTOpt} from "../../typechain-types/contracts/NFTOpt";
-import {Option, OptionWithNFTDetails} from "./types";
-import {addressEmpty, SECONDS_IN_A_DAY} from "./constants";
-import {fetchNFTImage} from "./NFT/localhost";
+import { BigNumber } from "ethers";
+import { NFTOpt } from "../../typechain-types/contracts/NFTOpt";
+import { Option, OptionWithNFTDetails } from "./types";
+import { addressEmpty, SECONDS_IN_A_DAY } from "./constants";
+import { fetchNFTImage } from "./NFT/localhost";
+import { getSignedContract } from "./metamask";
 
 function isOptionValid(option: any): boolean {
     let isMalformed =
@@ -17,7 +18,7 @@ function isOptionValid(option: any): boolean {
     return !isMalformed;
 }
 
-export async function loadContractOptions(contract: NFTOpt): Promise<Option[]> {
+export async function loadoptionSoliditys(contract: NFTOpt): Promise<Option[]> {
     let options: Option[] = [];
 
     try {
@@ -31,24 +32,24 @@ export async function loadContractOptions(contract: NFTOpt): Promise<Option[]> {
         const optionsLength = optionIDPromise.toNumber();
 
         for (let idx = 1; idx <= optionsLength; ++idx) {
-            const contractOption = await contract.options(idx);
+            const optionSolidity = await contract.options(idx);
 
-            if (!isOptionValid(contractOption)) {
+            if (!isOptionValid(optionSolidity)) {
                 continue;
             }
 
             options.push({
                 id: idx,
-                buyer: contractOption.buyer.toLowerCase(),
-                seller: contractOption.seller.toLowerCase(),
-                flavor: contractOption.flavor,
-                interval: contractOption.interval / SECONDS_IN_A_DAY,
-                nftContract: contractOption.nftContract,
-                nftId: contractOption.nftId,
-                premium: contractOption.premium,
-                startDate: contractOption.startDate.toNumber(),
-                state: contractOption.state,
-                strikePrice: contractOption.strikePrice,
+                buyer: optionSolidity.buyer.toLowerCase(),
+                seller: optionSolidity.seller.toLowerCase(),
+                flavor: optionSolidity.flavor,
+                interval: optionSolidity.interval / SECONDS_IN_A_DAY,
+                nftContract: optionSolidity.nftContract,
+                nftId: optionSolidity.nftId,
+                premium: optionSolidity.premium,
+                startDate: optionSolidity.startDate.toNumber(),
+                state: optionSolidity.state,
+                strikePrice: optionSolidity.strikePrice,
             });
         }
     } catch (err) {
@@ -58,35 +59,51 @@ export async function loadContractOptions(contract: NFTOpt): Promise<Option[]> {
     return options;
 }
 
-export async function loadContractOptionWithNFTDetails(
+export async function loadOptionWithaAsset(
     contract: NFTOpt,
     optionId: number
 ): Promise<OptionWithNFTDetails> {
     let option: OptionWithNFTDetails = null;
 
     try {
-        const contractOption = await contract.options(optionId);
+        const optionSolidity = await contract.options(optionId);
 
-        if (!isOptionValid(contractOption)) {
-            return;
-        }
+        if (!isOptionValid(optionSolidity)) { return; }
+
+        const abi_IERC721 = [
+            {
+                "inputs": [],
+                "name": "name",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+        ];
+
+        const NFTContract = getSignedContract(optionSolidity.nftContract, abi_IERC721);
 
         option = {
             id: optionId,
-            buyer: contractOption.buyer.toLowerCase(),
-            seller: contractOption.seller.toLowerCase(),
-            flavor: contractOption.flavor,
-            interval: contractOption.interval / SECONDS_IN_A_DAY,
-            premium: contractOption.premium,
-            startDate: contractOption.startDate.toNumber(),
-            state: contractOption.state,
-            strikePrice: contractOption.strikePrice,
+            buyer: optionSolidity.buyer.toLowerCase(),
+            seller: optionSolidity.seller.toLowerCase(),
+            flavor: optionSolidity.flavor,
+            interval: optionSolidity.interval / SECONDS_IN_A_DAY,
+            premium: optionSolidity.premium,
+            startDate: optionSolidity.startDate.toNumber(),
+            state: optionSolidity.state,
+            strikePrice: optionSolidity.strikePrice,
             asset: {
-                tokenId: contractOption.nftId,
-                address: contractOption.nftContract,
-                name: "X Collection - " + contractOption.nftId.toNumber(),
-                image: await fetchNFTImage(contractOption.nftContract, contractOption.nftId),
-                url: "",
+                id: optionId,
+                tokenId: optionSolidity.nftId,
+                address: optionSolidity.nftContract,
+                name: await NFTContract.name() + " - " + optionSolidity.nftId.toString(),
+                image: await fetchNFTImage(optionSolidity.nftContract, optionSolidity.nftId),
             },
         };
     } catch (err) {
