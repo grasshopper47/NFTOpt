cf141e9ca9e26078055ea8e1df4a09fb251aa602
Stefana Mitrea <mitreastefanaa@gmail.com>
2022-05-28 18:29:35 +0300

    update options when transaction event is received, fix random bugs, ui fixes create page, hide dummy seller address

--- src/pages/my-options.tsx
+++ src/pages/my-options.tsx
@@ -1,42 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "../components/Layout";
 import OptionsListContainer from "../components/OptionsListContainer";
-import { Option, OptionState, OptionWithNFTDetails } from "../utils/types";
-import { useAccount, useContracts } from "../providers/contexts";
-import { fetchNFTDetailsForMultipleOptions } from "../utils/NFT/localhost";
-import { loadContractOptions } from "../utils/options";
+import {OptionFilterOwnership} from "../utils/types";
 
 function MyOptions() {
-    const account = useAccount();
-    const { nftOpt } = useContracts();
-
-    const [contractOptions, setContractOptions] = useState<Option[]>([]);
-    const [currentAccountOptions, setCurrentAccountOptions] = useState<Option[]>([]);
-    const [optionsWithNFTDetails, setOptionsWithNFTDetails] = useState<OptionWithNFTDetails[]>([]);
-
-    // Firstly load the contract options
-    useEffect(() => {
-        loadContractOptions(nftOpt, setContractOptions);
-    }, [nftOpt]);
-
-    // Then filter the options for the current account => current account is either seller or buyer
-    useEffect(() => {
-        setCurrentAccountOptions(
-            contractOptions.filter((option) =>
-                option.buyer === account ||
-                option.seller === account)
-        );
-    }, [contractOptions, account]);
-
-    // console.log(contractOptions.filter((option) => option.buyer === account || option.seller === account));
-    // Then fetch the NFT details for the current account options
-    useEffect(() => {
-        fetchNFTDetailsForMultipleOptions(currentAccountOptions, setOptionsWithNFTDetails);
-    }, [currentAccountOptions]);
-
     return (
         <Layout>
-            <OptionsListContainer key={`my-options-list`} title={"Your NFT Options"} options={optionsWithNFTDetails} />
+            <OptionsListContainer
+                key={`my-options-list`}
+                title={"Your NFT Options"}
+                filterOwnership={OptionFilterOwnership.PERSONAL}
+            />
         </Layout>
     );
 }
--- src/utils/options.ts
+++ src/utils/options.ts
@@ -1,7 +1,8 @@
-import { BigNumber, ethers } from "ethers";
-import { NFTOpt } from "../../typechain-types/contracts/NFTOpt";
-import { Option } from "./types";
-import { addressEmpty, SECONDS_IN_A_DAY } from "./constants";
+import {BigNumber, ethers} from "ethers";
+import {NFTOpt} from "../../typechain-types/contracts/NFTOpt";
+import {Option, OptionWithNFTDetails} from "./types";
+import {addressEmpty, SECONDS_IN_A_DAY} from "./constants";
+import {fetchNFTImage} from "./NFT/localhost";
 
 function isOptionValid(option: any): boolean {
     let isMalformed =
@@ -16,35 +17,81 @@ function isOptionValid(option: any): boolean {
     return !isMalformed;
 }
 
-export async function loadContractOptions(contract: NFTOpt, setOptionsCallback: (options: Option[]) => void) {
+export async function loadContractOptions(contract: NFTOpt): Promise<Option[]> {
     let options: Option[] = [];
 
-    const optionIDPromise = await contract.optionID();
+    try {
+        const optionIDPromise = await contract.optionID();
 
-    if (!optionIDPromise) { return; }
+        if (!optionIDPromise) {
+            return;
+        }
 
-    // TODO: handle optionsLength > 2^53
-    const optionsLength = optionIDPromise.toNumber();
+        // TODO: handle optionsLength > 2^53
+        const optionsLength = optionIDPromise.toNumber();
 
-    for (let idx = 1; idx !== optionsLength; ++idx) {
-        const contractOption = await contract.options(idx);
+        for (let idx = 1; idx <= optionsLength; ++idx) {
+            const contractOption = await contract.options(idx);
 
-        if (!isOptionValid(contractOption)) { continue; }
+            if (!isOptionValid(contractOption)) {
+                continue;
+            }
 
-        options.push({
-            id: idx,
+            options.push({
+                id: idx,
+                buyer: contractOption.buyer.toLowerCase(),
+                seller: contractOption.seller.toLowerCase(),
+                flavor: contractOption.flavor,
+                interval: contractOption.interval / SECONDS_IN_A_DAY,
+                nftContract: contractOption.nftContract,
+                nftId: contractOption.nftId,
+                premium: contractOption.premium,
+                startDate: contractOption.startDate.toNumber(),
+                state: contractOption.state,
+                strikePrice: contractOption.strikePrice,
+            });
+        }
+    } catch (err) {
+        console.error(err);
+    }
+
+    return options;
+}
+
+export async function loadContractOptionWithNFTDetails(
+    contract: NFTOpt,
+    optionId: number
+): Promise<OptionWithNFTDetails> {
+    let option: OptionWithNFTDetails = null;
+
+    try {
+        const contractOption = await contract.options(optionId);
+
+        if (!isOptionValid(contractOption)) {
+            return;
+        }
+
+        option = {
+            id: optionId,
             buyer: contractOption.buyer.toLowerCase(),
             seller: contractOption.seller.toLowerCase(),
             flavor: contractOption.flavor,
             interval: contractOption.interval / SECONDS_IN_A_DAY,
-            nftContract: contractOption.nftContract,
-            nftId: contractOption.nftId,
             premium: contractOption.premium,
             startDate: contractOption.startDate.toNumber(),
             state: contractOption.state,
             strikePrice: contractOption.strikePrice,
-        });
+            asset: {
+                tokenId: contractOption.nftId,
+                address: contractOption.nftContract,
+                name: "X Collection - " + contractOption.nftId.toNumber(),
+                image: await fetchNFTImage(contractOption.nftContract, contractOption.nftId),
+                url: "",
+            },
+        };
+    } catch (err) {
+        console.error(err);
     }
 
-    setOptionsCallback(options);
+    return option;
 }
