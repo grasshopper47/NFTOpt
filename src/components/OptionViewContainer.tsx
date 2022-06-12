1bbbdbb125ee39e82f564c71805e40397cadb82f
grasshopper47 <a@a>
2022-06-12 23:25:13 +0200

    Used Options context

--- src/components/OptionViewContainer.tsx
+++ src/components/OptionViewContainer.tsx
@@ -1,11 +1,10 @@
+import classes from "./styles/OptionViewContainer.module.scss";
 import { Tab, Tabs } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
-import { OptionFilterOwnership, OptionState, OptionWithAsset } from "../utils/types";
 import OptionDetailsView from "./OptionDetailsView";
 import OptionListItemView from "./OptionListItemView";
-import classes from "./styles/OptionViewContainer.module.scss";
-import { useAccount, useContracts } from "../providers/contexts";
-import { loadOptionsWithAsset } from "../utils/options";
+import { useEffect, useState } from "react";
+import { OptionFilterOwnership, OptionState, OptionWithAsset } from "../utils/types";
+import { useAccount, useOptions } from "../providers/contexts";
 
 type OptionViewContainerProps =
 {
@@ -13,82 +12,64 @@ type OptionViewContainerProps =
     filterOwnership: OptionFilterOwnership;
 };
 
-type OptionStateTab =
+type ViewTab =
 {
-    name: string;
-    optionState: OptionState;
+    name  : string;
+    value : OptionState;
 };
 
-const optionStateTabs: OptionStateTab[] =
+const tabs: ViewTab[] =
 [
     {
-        name: "Requests",
-        optionState: OptionState.REQUEST,
+        name  : "Requests",
+        value : OptionState.REQUEST,
     },
     {
-        name: "Open",
-        optionState: OptionState.OPEN,
+        name  : "Open",
+        value : OptionState.OPEN,
     },
     {
-        name: "Closed",
-        optionState: OptionState.CLOSED,
+        name  : "Closed",
+        value : OptionState.CLOSED,
     },
 ];
 
-function OptionViewContainer(props: OptionViewContainerProps) {
-    const { title, filterOwnership } = props;
+function OptionViewContainer(props: OptionViewContainerProps)
+{
+    const { title } = props;
+
+    const [ activeTabIndex, setActiveTabIndex ] = useState(0);
+    const [ optionsDisplayed, setOptionsDisplayed ] = useState<OptionWithAsset[]>([]);
+    const [ selectedOption, setSelectedOption ] = useState<OptionWithAsset | null>(null);
 
     const account = useAccount();
-    const { nftOpt } = useContracts();
-
-    const [activeTabIndex, setActiveTabIndex] = useState(0);
-
-    const [optionsWithAsset, setOptionsWithAsset] = useState<OptionWithAsset[]>([]);
-    const [filteredOptions, setFilteredOptions] = useState<OptionWithAsset[]>([]);
-    const [selectedOption, setSelectedOption] = useState<OptionWithAsset | null>(null);
-    const lastSelectedOptionId = useRef<number | null>(null); // TODO: make an array for options in progress
+    const options = useOptions();
 
     useEffect
     (
         () =>
         {
-            loadOptionsWithAsset(nftOpt)
-            .then
-            (
-                (options) =>
+            if (!options || options.length === 0) { return; }
+
+            const state = tabs[activeTabIndex].value;
+
+            const filter =
+            (option : OptionWithAsset) =>
+            {
+                if (state === OptionState.OPEN || state === OptionState.REQUEST)
                 {
-                    console.log("loaded", options);
-                    setOptionsWithAsset(options);
+                    return option.state === state;
                 }
-            );
+
+                return option.state === OptionState.WITHDRAWN || option.state === OptionState.CLOSED;
+            };
+
+            setOptionsDisplayed(options.filter(filter));
         },
-    []
-    );
-
-    // Filter by active tab
-    useEffect
-    (
-        () =>
-        {
-            const state = optionStateTabs[activeTabIndex].optionState;
-
-            setFilteredOptions
-            (
-                optionsWithAsset?.filter
-                (
-                    (option) =>
-                    state === OptionState.OPEN || state === OptionState.REQUEST
-                        ? option.state === state
-                        : option.state === OptionState.WITHDRAWN || option.state === OptionState.CLOSED
-                )
-            );
-
-            if (selectedOption) { setSelectedOption(null); }
-        },
-    [
-        activeTabIndex,
-        optionsWithAsset
-    ]
+        [
+            activeTabIndex,
+            options
+        ]
     );
 
     const handleChangeTab = (_, tabIndex: number) => setActiveTabIndex(tabIndex);
@@ -96,8 +77,12 @@ function OptionViewContainer(props: OptionViewContainerProps) {
     return (
         <div className={classes.root}>
             <p className={classes.title}>{title}</p>
-            <Tabs className={classes.tabs} value={activeTabIndex} onChange={handleChangeTab}>
-                {optionStateTabs.map((optionStateTab) => (
+            <Tabs
+                className={classes.tabs}
+                value={activeTabIndex}
+                onChange={handleChangeTab}
+            >
+                {tabs.map((optionStateTab) => (
                     <Tab key={`option-state-tab-${optionStateTab.name}`} label={optionStateTab.name} />
                 ))}
             </Tabs>
@@ -113,8 +98,8 @@ function OptionViewContainer(props: OptionViewContainerProps) {
                 </div>
             ) : (
                 <div className={classes.containerGrid}>
-                    {filteredOptions?.length ? (
-                        filteredOptions?.map((option, index) => (
+                    { optionsDisplayed?.length ? (
+                        optionsDisplayed?.map((option, index) => (
                             <OptionListItemView
                                 key={`option-preview-${activeTabIndex}-${index}`}
                                 option={option}
@@ -123,7 +108,7 @@ function OptionViewContainer(props: OptionViewContainerProps) {
                         ))
                     ) : (
                         <p className={classes.noOptions}>No Options</p>
-                    )}
+                    ) }
                 </div>
             )}
         </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
-import { AppProps } from "next/app";
 import "./_app.scss";
-import { AccountContext, ContractsContext } from "../providers/contexts";
+import { useState, useEffect, useRef } from "react";
+import { AppProps } from "next/app";
+import { AccountContext, ContractsContext, OptionsContext } from "../providers/contexts";
 import { NETWORK_NAME, TOAST_DURATION } from "../utils/constants";
 import NFTOptSolContract from "../../artifacts/contracts/NFTOpt.sol/NFTOpt.json";
 import addresses from "../../addresses.json";
@@ -9,67 +9,118 @@ import Header from "../components/Header";
 import toast, { Toaster } from "react-hot-toast";
 import RouteGuard from "../components/RouteGuard";
 import { NFTOpt } from "../../typechain-types";
-import {
-    hookUpMetamask,
-    getSignedContract,
-    getCurrentAccount,
-    connectWallet,
-    getCurrentProvider,
-} from "../utils/metamask";
+import { hookUpMetamask, getSignedContract, getCurrentAccount, getCurrentProvider, connectWallet } from "../utils/metamask";
 import { dismissLastToast } from "../utils/frontend";
+import { OptionState, OptionWithAsset } from "../utils/types";
+import { loadOptionWithAssetDetails, loadOptionsWithAsset } from "../utils/options";
 
 export default function App({ Component, pageProps }: AppProps) {
-    const [account, setAccount] = useState(null);
     const [loaded, setLoaded] = useState(false);
+    const [account, setAccount] = useState(null);
     const [contracts, setContracts] = useState(null);
+    const [options, setOptions] = useState<OptionWithAsset[]>([]);
     const blockNo = useRef<number>(0);
 
-    const success = (message: string, newBlockNo: number = 0) => {
+    function onContractEvent
+    (
+        contract   : NFTOpt
+    ,   action     : string
+    ,   optionID   : number
+    ,   newBlockNo : number = 0
+    )
+    {
+        // Filter out old events which are re-emitted when intitialized, then emitted new event
+        if (blockNo.current >= newBlockNo) { return; }
+
+        blockNo.current = newBlockNo;
+
         dismissLastToast();
 
-        // Filter out old events which are re-emitted when intitialized, then emitted new event
-        if (blockNo.current < newBlockNo) {
-            blockNo.current = newBlockNo;
-            toast.success("Successfully " + message, { duration: TOAST_DURATION });
-        }
-    };
+        toast.success("Successfully " + action, { duration: TOAST_DURATION });
 
-    let onContractEvent = (action: string, blockNo: number) => {
-        success(`${action} request`, blockNo);
         console.log(action);
+
+        // For new options, reload details after publishing
+        if (action[0] === "p")
+        {
+            loadOptionWithAssetDetails(contract, optionID).then( option => setOptionsAndUpdateListeners([...options, option]) );
+
+            return;
+        }
+
+        // For existing options, update state
+        for (const o of options)
+        {
+            if (o.id !== optionID) { continue; }
+
+            if (action[0] === "w") { o.state = OptionState.WITHDRAWN; break; }
+            if (action[0] === "o") { o.state = OptionState.OPEN; break; }
+            if (action[0] === "c") { o.state = OptionState.CLOSED; break; }
+            if (action[0] === "e") { o.state = OptionState.CLOSED; break; }
+        }
+
+        setOptionsAndUpdateListeners([...options]);
     }
 
-    const attachEventListeners = (contract: NFTOpt) => {
-        contract.on("NewRequest", (id, tx) => onContractEvent("published", tx.blockNumber) );
-        contract.on("Withdrawn" , (id, tx) => onContractEvent("withdrawn", tx.blockNumber) );
-        contract.on("Opened"    , (id, tx) => onContractEvent("opened"   , tx.blockNumber) );
-        contract.on("Canceled"  , (id, tx) => onContractEvent("canceled" , tx.blockNumber) );
-        contract.on("Exercised" , (id, tx) => onContractEvent("exercised", tx.blockNumber) );
+    function attachEventListeners(contract: NFTOpt)
+    {``
+        contract.on("NewRequest", (id, tx) => onContractEvent(contract, "published", id.toNumber(), tx.blockNumber));
+        contract.on("Withdrawn" , (id, tx) => onContractEvent(contract, "withdrawn", id.toNumber(), tx.blockNumber));
+        contract.on("Opened"    , (id, tx) => onContractEvent(contract, "opened"   , id.toNumber(), tx.blockNumber));
+        contract.on("Canceled"  , (id, tx) => onContractEvent(contract, "canceled" , id.toNumber(), tx.blockNumber));
+        contract.on("Exercised" , (id, tx) => onContractEvent(contract, "exercised", id.toNumber(), tx.blockNumber));
     };
 
-    useEffect(() => {
-        hookUpMetamask()
-        .then( () => {
-            getCurrentProvider().getBlockNumber().then(bn => blockNo.current = bn);
+    function setOptionsAndUpdateListeners(options)
+    {
+        setOptions([...options]);
 
-            const contract = getSignedContract(addresses[NETWORK_NAME].NFTOpt, NFTOptSolContract.abi) as NFTOpt;
+        if (!contracts) { return; }
 
-            attachEventListeners(contract);
+        contracts.nftOpt.removeAllListeners();
+        attachEventListeners(contracts.nftOpt);
+    }
 
-            setContracts({ nftOpt: contract });
-            setAccount(getCurrentAccount());
-            setLoaded(true);
-        });
-    }, []);
+    useEffect
+    (
+        () =>
+        {
+            const onLoad = async () =>
+            {
+                await hookUpMetamask();
+                setAccount(getCurrentAccount());
+
+                const contract =
+                getSignedContract
+                (
+                    addresses[NETWORK_NAME].NFTOpt
+                ,   NFTOptSolContract.abi
+                ) as NFTOpt;
+
+                setContracts({ nftOpt : contract });
+
+                getCurrentProvider().getBlockNumber().then( bn => blockNo.current = bn );
+
+                loadOptionsWithAsset(contract).then( r => setOptionsAndUpdateListeners([...r]) );
+
+                setLoaded(true);
+            }
+
+            onLoad();
+        },
+        []
+    );
 
     return (
         <AccountContext.Provider value={account}>
             <ContractsContext.Provider value={contracts}>
-                <RouteGuard account={account} loaded={loaded}>
-                    <Toaster containerClassName={"toast-container"} />
-                    <Header account={account} onConnectAccount={connectWallet.bind(null, setAccount)} />
-                    <Component {...pageProps} />
-                </RouteGuard>
+                <OptionsContext.Provider value={options}>
+                    <RouteGuard account={account} loaded={loaded}>
+                        <Toaster containerClassName={"toast-container"} />
+                        <Header account={account} onConnectAccount={connectWallet.bind(null, setAccount)} />
+                        <Component {...pageProps} />
+                    </RouteGuard>
+                </OptionsContext.Provider>
             </ContractsContext.Provider>
         </AccountContext.Provider>
     );
