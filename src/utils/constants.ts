1aa15f8136a2e88a3f43136a289948750673e183
grasshopper47 <a@a>
2022-06-20 15:57:31 +0200

    Added BIGNUMBER0

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,10 @@
+import { BigNumber } from "ethers";
+
 const _networks : any =
 {
-    "1": "mainnet",
-    "4": "rinkeby",
-    "31337": "localhost",
+    "1"     : "mainnet"
+,   "4"     : "rinkeby"
+,   "31337" : "localhost"
 };
 
 const _network_id: string = process.env.NEXT_PUBLIC_NETWORK_ID || "31337";
@@ -10,6 +12,7 @@ const _network_id: string = process.env.NEXT_PUBLIC_NETWORK_ID || "31337";
 export const floatNumberRegex = /^([0-9]*[.])?[0-9]*$/;
 
 export const ADDRESS0: string = "0x0000000000000000000000000000000000000000";
+export const BIGNUMBER0 : BigNumber = BigNumber.from(0);
 export const NETWORK_NAME: string = _networks[_network_id];
 export const SECONDS_IN_A_DAY = 86400;
 export const TOAST_DURATION = 2000;
