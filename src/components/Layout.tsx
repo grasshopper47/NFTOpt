147b2d7fafe0954e4409b975ff53f2769bfc1d85
grasshopper47 <a@a>
2022-06-23 09:03:28 +0200

    Cleaned and reformatted

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { WithChildren } from "../utils/types";
 
-function Layout(props: WithChildren) {
-    return (
+function Layout(props) {
+    return <>
         <div key="main-content-layout" className="main-content-layout">
             <div className="main-content-wrapper">{props.children}</div>
         </div>
-    );
+    </>;
 }
 
 export default Layout;
