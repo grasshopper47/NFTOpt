8f6f4964249a5c4c4e326a71eeab09dc1cabf30d
grasshopper47 <a@a>
2022-07-09 23:29:13 +0200

    Improve allocations

--- src/frontend/components/TableView.tsx
+++ src/frontend/components/TableView.tsx
@@ -8,8 +8,6 @@ import RowView from "./RowView";
 enum SortMode { ASCENDING, DESCENDING };
 let sortMode = SortMode.DESCENDING;
 
-let updateListHash : () => void;
-
 type Props =
 {
     list           : OptionWithAsset[]
@@ -20,43 +18,45 @@ type Props =
 
 export const TableViewStates = [ "25", "50", "100" ];
 
+let sortList : (sorter : (a1: OptionWithAsset, a2: OptionWithAsset) => number) => void;
+
+let header =
+(
+    <div className={classes.listRowsHeader}>
+        <p onClick={ () => sortList( (a, b) => b.id - a.id ) }
+        >#</p>
+
+        <p onClick={ () => sortList( (a, b) => b.asset.name.localeCompare(a.asset.name) ) }
+        >Name</p>
+
+        <p onClick={ () => sortList( (a, b) => b.premium.toString().localeCompare(a.premium.toString()) ) }
+        >Premium</p>
+
+        <p onClick={ () => sortList( (a, b) => b.strikePrice.toString().localeCompare(a.strikePrice.toString()) ) }
+        >Strike Price</p>
+
+        <p onClick={ () => sortList( (a, b) => b.interval - a.interval ) }
+        >Interval</p>
+    </div>
+);
+
 function TableView(props: Props)
 {
-    const [ listHash , setListHash ] = useState(0);
-
     // Used to sort the options in place
-    updateListHash = () => setListHash( h => ++h );
+    const [ listHash , setListHash ] = useState(0);
 
     let selectedValueID = props.selectedValue ? props.selectedValue.id : -1;
 
-    let sortList = (sorter : (a1: OptionWithAsset, a2: OptionWithAsset) => number) =>
+    sortList = (sorter : (a1: OptionWithAsset, a2: OptionWithAsset) => number) =>
     {
         if (sortMode === SortMode.ASCENDING) { props.list.sort(sorter); sortMode = SortMode.DESCENDING; }
         else                                 { props.list.sort((a, b) => sorter(b, a)); sortMode = SortMode.ASCENDING; }
 
-        updateListHash();
+        setListHash( h => ++h );
     }
 
     return <div className={classes.containerGrid}>
-        {
-            props.list.length !== 0 &&
-            <div className={classes.listRowsHeader}>
-                <p onClick={ () => sortList( (a, b) => b.id - a.id ) }
-                >#</p>
-
-                <p onClick={ () => sortList( (a, b) => b.asset.name.localeCompare(a.asset.name) ) }
-                >Name</p>
-
-                <p onClick={ () => sortList( (a, b) => b.premium.toString().localeCompare(a.premium.toString()) ) }
-                >Premium</p>
-
-                <p onClick={ () => sortList( (a, b) => b.strikePrice.toString().localeCompare(a.strikePrice.toString()) ) }
-                >Strike Price</p>
-
-                <p onClick={ () => sortList( (a, b) => b.interval - a.interval ) }
-                >Interval</p>
-            </div>
-        }
+        { props.list.length !== 0 && header }
 
         {
             props.list.map
@@ -66,7 +66,7 @@ function TableView(props: Props)
                     key={`option-row-${option.id}`}
                     style={{width:"100%", display:"grid"}}
                     // If previously selected an option, and it is the same one, set it to null
-                    { ... props.onSelect && { onClick: () => props.onSelect(option.id === selectedValueID ? null : option) } }
+                    onClick={ () => props.onSelect(option.id === selectedValueID ? null : option) }
                 >
                     <RowView option={option} showDetails={option.id === selectedValueID} />
                 </div>
