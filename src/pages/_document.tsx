5bd7541c8ef1b85438e434e12fb719731848e1f4
grasshopper47 <a@a>
2022-07-17 22:44:53 +0200

    Formatting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,20 +5,18 @@ export default class Document extends NextDocument
 {
     render()
     {
-        return <>
-            <Html>
-                <Head>
-                    <meta name="description" content="A lightweight marketplace for NFT Options"></meta>
-                    <link
-                        rel="icon"
-                        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎭</text></svg>"
-                    />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        </>;
+        return <Html>
+            <Head>
+                <meta name="description" content="A lightweight marketplace for NFT Options"></meta>
+                <link
+                    rel="icon"
+                    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎭</text></svg>"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>;
     }
 }
--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import Layout from "../frontend/fragments/Layout";
 import ViewContainer from "../frontend/components/ViewContainer";
 
 export default function()
 {
-    return <>
-        <Layout>
-            <ViewContainer />
-        </Layout>
-    </>;
+    return <Layout>
+        <ViewContainer />
+    </Layout>;
 }
