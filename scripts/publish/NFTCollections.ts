13bd1381fa92e9d51afb3d821cfa1dcf5c9adbb6
grasshopper47 <a@a>
2022-08-11 19:43:12 +0200

    Create JSON file to be used by graph-compile

--- scripts/publish/NFTCollections.ts
+++ scripts/publish/NFTCollections.ts
@@ -1,5 +1,6 @@
 import { deployNFTCollectionContract, NFTCollectionContracts } from "../../utils/deployment/NFTCollections";
 import fs from "fs";
+import { ethers } from "hardhat";
 
 const addressesPath = "addresses.json";
 
@@ -13,6 +14,15 @@ export async function publishNFTCollections()
         const data = fs.readFileSync(addressesPath, { encoding : "utf8", flag : "r" });
         addressesJSON = JSON.parse(data.toString());
     }
+
+    // Create graphs/config.json
+    let graphConfigJSON = {
+        output: "./graphs/generated/ERC721/",
+        chain: "localhost",
+        datasources: []
+    };
+
+    let blockNumber = await ethers.provider.getBlockNumber();
 
     let promises : Promise<any>[] = [];
 
@@ -28,6 +38,12 @@ export async function publishNFTCollections()
                     // @ts-ignore
                     addressesJSON.localhost[name] = NFTCollectionContracts[name].instance.address;
 
+                    graphConfigJSON.datasources.push
+                    (
+                        // @ts-ignore
+                        { id: name, address: addressesJSON.localhost[name], startBlock: blockNumber, module: [ "ERC721" ] }
+                    )
+
                     // @ts-ignore
                     console.log(`Deployed ${name} @ ${addressesJSON.localhost[name]}`);
                 }
@@ -39,6 +55,9 @@ export async function publishNFTCollections()
 
     // Update addresses.json file with published contracts addresses
     fs.writeFileSync("addresses.json", JSON.stringify(addressesJSON));
+
+    // Update graphs/config.json file
+    fs.writeFileSync("graphs/config.json", JSON.stringify(graphConfigJSON));
 }
 
 publishNFTCollections()
