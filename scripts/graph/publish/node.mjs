619fa192293404f09a2b8a0b1bda9518b73c7d21
grasshopper47 <a@a>
2022-08-14 00:12:17 +0200

    Auto-create node

--- scripts/graph/publish/node.mjs
+++ scripts/graph/publish/node.mjs
@@ -9,16 +9,54 @@ function pubishNode()
     let version = process.argv[3] ?? "v0.0.1";
 
     let result;
+    fetch
+    (
+        `http://127.0.0.1:8000/subgraphs/name/${name}`
+    ,   {
+            method: "POST"
+        ,   body : JSON.stringify( { query: "query { test123 }" } )
+        }
+    )
+    .then(res => res.json())
+    .then
+    (
+        (r) =>
+        {
+            if (r.errors.length === 1 && r.errors[0].message.search("does not exist") !== -1)
+            {
+                try { result = execSync (`graph create --node ${graphIndexerNode} ${name}`); }
+                catch (e)
+                {
+                    console.log(e.stdout.toString());
 
-    try { result = execSync (`cd graphs/${name}/generated; graph deploy ${name} --version-label ${version} --ipfs ${ipfsNode} --node ${graphIndexerNode}`); }
-    catch (e)
-    {
-        console.log(e.stdout.toString());
+                    return;
+                }
 
-        return;
-    }
+                console.log(result.toString());
+            }
 
-    console.log(result.toString());
+            try
+            {
+                result = execSync
+                (`
+                    cd graphs/${name}/generated;  \
+                                                  \
+                    graph deploy ${name}          \
+                    --version-label ${version}    \
+                    --ipfs ${ipfsNode}            \
+                    --node ${graphIndexerNode}
+                `);
+            }
+            catch (e)
+            {
+                console.log(e.stdout.toString());
+
+                return;
+            }
+
+            console.log(result.toString());
+        }
+    )
 }
 
 pubishNode();
