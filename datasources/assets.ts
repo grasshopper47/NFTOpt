4faa621a22d127d0d3689a39a4eb7fc4c504e1c6
grasshopper47 <a@a>
2022-08-14 04:13:27 +0200

    Fetch assets from Graph + known network addresses

--- datasources/assets.ts
+++ datasources/assets.ts
@@ -6,12 +6,10 @@ import { getCachedContract } from "./ERC-721/contracts";
 import { MAX_MINTABLE_TOKENS } from "../utils/constants";
 import addresses from "../addresses.json";
 
-let assets = {};
-
-export const clearAssets = () => assets = {};
+export const clearAssets   = () => assets = {};
 export const clearAssetsOf = (account : string) => assets[account] = [] as NFTAsset[];
-export const assetsOf = (account : string) => assets[account] as NFTAsset[];
-export const addAssetTo = (account: string, obj : NFTAsset) => assets[account].push(obj);
+export const assetsOf      = (account : string) => assets[account] as NFTAsset[];
+export const addAssetTo    = (account: string, obj : NFTAsset) => assets[account].push(obj);
 
 export const addAssetByKeyTo = async (account: string, key : AssetKey) =>
 {
@@ -42,36 +40,76 @@ export const getNFTAsset = async (key : AssetKey, contract? : any) =>
 
 export const loadAssetsFor = async (account: string) =>
 {
-    if (!account || assets[account]) return;
+    if (!account || account === " " || assets[account]) return;
 
     console.log("loadAssetsFor", account);
 
     let arr      : NFTAsset[]     = [];
     let promises : Promise<any>[] = [];
 
-    for (let name of Object.keys(addresses.localhost))
-    {
-        if (name === "NFTOpt") continue;
+    let isMissing = false;
 
-        for (let i = 1; i != MAX_MINTABLE_TOKENS; ++i)
+    let reply = await fetch
+    (
+        "http://127.0.0.1:8000/subgraphs/name/NFTCollections"
+    ,   {
+            method : "POST"
+        ,   body   : JSON.stringify({ query: `{ account (id:"${account}") { tokens { identifier contract } } }` })
+        }
+    )
+    .catch( () => { return { json: () => "{}" } } );
+
+    let json = await reply.json();
+
+    if (json.errors)
+    {
+        for (let e of json.errors) console.error(e.message);
+
+        // Force loading from localhost on Graph query error
+        json = "{}";
+    }
+
+    if (json !== "{}")
+    {
+        // Load from Graph query result
+        for (let t of json.data.account.tokens)
         {
             promises.push
             (
-                (async (key : AssetKey) =>
-                {
-                    let contract = getCachedContract(key.nftContract);
-
-                    let owner = await contract.ownerOf(key.nftId);
-                    if (owner !== account) return;
-
-                    let asset = await getNFTAsset(key, contract);
-                    arr.push(asset);
-                })
-                ({
-                    nftId       : i.toString()
-                ,   nftContract : addresses.localhost[name]
-                })
+                _loadAssetByKey
+                (
+                    {
+                        nftId       : t.identifier
+                    ,   nftContract : t.contract
+                    }
+                ,   account
+                ,   arr
+                )
             );
+        }
+    }
+    else
+    {
+        // Load from known addresses -- produces errors for missing token IDs
+        for (let name of Object.keys(addresses.localhost))
+        {
+            if (name === "NFTOpt") continue;
+
+            for (let i = 1; i != MAX_MINTABLE_TOKENS; ++i)
+            {
+                promises.push
+                (
+                    _loadAssetByKey
+                    (
+                        {
+                            nftId       : i.toString()
+                        ,   nftContract : addresses.localhost[name]
+                        }
+                    ,   account
+                    ,   arr
+                    )
+                );
+            }
         }
     }
 
@@ -81,3 +119,16 @@ export const loadAssetsFor = async (account: string) =>
 
     return arr;
 }
+
+async function _loadAssetByKey(key : AssetKey, account : string, arr : NFTAsset[])
+{
+    let contract = getCachedContract(key.nftContract);
+
+    let owner = await contract.ownerOf(key.nftId);
+    if (owner !== account) return;
+
+    let asset = await getNFTAsset(key, contract);
+    arr.push(asset);
+}
+
+let assets = {};
