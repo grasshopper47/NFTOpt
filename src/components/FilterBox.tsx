0570555350234e647e9e3509c1495849684a6ae2
grasshopper47 <a@a>
2022-08-21 18:15:40 +0200

    Added dropdown with option flavor to FilterBox

--- src/components/FilterBox.tsx
+++ src/components/FilterBox.tsx
@@ -4,23 +4,43 @@ import clsx from "clsx";
 
 import React, { useState } from "react";
 import { filterParams, resetFilterParams } from "../../datasources/filtering";
+import { OptionFlavor } from "../../models/enums";
 import { connected } from "../utils/metamask";
 import { getFloatString, getIntervalString } from "../utils/helpers";
 import { useAccount } from "../utils/contexts";
+import { flavorLabels } from "../utils/labels";
 import Row_FilterBox from "../fragments/Row.FilterBox";
-import { Button, FormControlLabel, Switch } from "@mui/material";
+import { Button, FormControlLabel, Select, SelectChangeEvent, Switch } from "@mui/material";
+
+const handleReset = () =>
+{
+    resetFilterParams();
+
+    _propsPtr.onFilter();
+}
+
+const handleFlavorChanged = (e : SelectChangeEvent) =>
+{
+    filterParams.flavor = parseInt(e.target.value) as OptionFlavor | -1;
+
+    _propsPtr.onFilter();
+}
+
+let account : string;
 
 type Props =
 {
     onFilter : () => void
 };
 
-let account : string;
+let _propsPtr : Props;
 
 resetFilterParams();
 
 function FilterBox(props : Props)
 {
+    _propsPtr = props;
+
     const [ showAll , setShowAll ] = useState(filterParams.account === "");
 
     account = useAccount();
@@ -58,7 +78,7 @@ function FilterBox(props : Props)
                 }
                 <Button
                     className={classes.btnReset}
-                    onClick={ () => { resetFilterParams(); props.onFilter(); } }
+                    onClick={handleReset}
                 >🧹</Button>
             </div>
 
@@ -79,6 +99,16 @@ function FilterBox(props : Props)
                 onValidate={getIntervalString}
                 value={filterParams.interval}
                 label="Interval" />
+
+            <Select
+                native
+                value={filterParams.flavor as any as string}
+                className={classes.select}
+                onChange={handleFlavorChanged}
+            >
+                <option value={-1}>Any Flavor</option>
+                { flavorLabels.map( (flavor, i) => <option key={`fb-flavors-${i}`} value={i}>{flavor}</option> ) }
+            </Select>
         </div>
     </div>;
 }
