f2d4b57ff7b68ec0e98b7bb442b354bc603f8f35
grasshopper47 <a@a>
2022-08-22 21:38:30 +0200

    Improved layout

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -5,35 +5,79 @@ import React from 'react';
 import Image from "next/image";
 import {team} from "../../datasources/team";
 import Layout from "../fragments/Layout";
+import { clsx } from "clsx";
 
 function TeamPage() {
-    return (
-        <Layout>
-            <div className={classes.root}>
-                {team.map((member, i) => (
-                    <div key={`member-card-${i}`} className={classes.card}>
-                        <Image
-                            src={`/images/${member.image}`}
-                            alt={member.name}
-                            width={70}
-                            height={74}
-                            layout="responsive"
-                        />
-                        <div className={classes.content}>
-                            {member.name ? <div className={classes.title}>{member.name}</div> : null}
-                            {member.social?.map(({name, url}, j) => (
-                                <div key={`social-${i}-${j}`} className={classes.social}>
-                                    <a href={url} target="_blank">
-                                        {name}
-                                    </a>
-                                </div>
-                            ))}
-                        </div>
+    return <Layout>
+        <div className={classes.root}>
+            {team.slice(0,4).map((member, i) => (
+                <div key={`member-card-${i}`} className={classes.card}>
+                    <Image
+                        src={`/images/${member.image}`}
+                        alt={member.name}
+                        style={{borderRadius:"6px 6px 0 0"}}
+                        width={70}
+                        height={74}
+                        layout="responsive"
+                    />
+                    <div className={classes.content}>
+                        {member.name ? <div className={classes.title}>{member.name}</div> : null}
+                        {member.social?.map(({name, url}, j) => (
+                            <div key={`social-${i}-${j}`} className={classes.social}>
+                                <a href={url} target="_blank">
+                                    {name}
+                                </a>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
+            ))}
+        </div>
+        <div className={clsx(classes.root, classes.cardGithub)}>
+            <div className={classes.card}>
+                <Image
+                    src="/images/github2.jpg"
+                    alt="NFTOpt"
+                    style={{borderRadius:"6px 6px 0 0"}}
+                    width={70}
+                    height={74}
+                    layout="responsive"
+                />
+                <div className={classes.content}>
+                    <div className={classes.title}>{"..  ✨ NFT-OթͲ ✨ .."}</div>
+                    <div className={classes.social}>
+                        <a href={"https://github.com/grasshopper47/NFTOpt"} target="_blank">
+                            Visit our repo
+                        </a>
+                    </div>
+                </div>
             </div>
-        </Layout>
-    );
+        </div>
+        <div className={classes.root}>
+            {team.slice(4).map((member, i) => (
+                <div key={`member-card-${i}`} className={classes.card}>
+                    <Image
+                        src={`/images/${member.image}`}
+                        alt={member.name}
+                        style={{borderRadius:"6px 6px 0 0"}}
+                        width={70}
+                        height={74}
+                        layout="responsive"
+                    />
+                    <div className={classes.content}>
+                        {member.name ? <div className={classes.title}>{member.name}</div> : null}
+                        {member.social?.map(({name, url}, j) => (
+                            <div key={`social-${i}-${j}`} className={classes.social}>
+                                <a href={url} target="_blank">
+                                    {name}
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    </Layout>;
 }
 
 export default TeamPage;
