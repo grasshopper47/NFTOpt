652b5425e5ab1b800673537df710eb4fabec3afd
grasshopper47 <a@a>
2022-08-22 13:09:55 +0200

    Added asset loading to app

--- src/utils/contexts.ts
+++ src/utils/contexts.ts
@@ -10,16 +10,21 @@ export const useChainID = () => useContext(ChainIDContext);
 export const ChainIDContext = createContext(0);
 export const AccountContext = createContext("");
 
+export const setAssetsLoadCallback    = (cb : AssetsLoaded)  => AssetsLoadCallback = cb;
+export const clearAssetsLoadCallback  = ()                   => AssetsLoadCallback = _emptyCB;
+
 export const setOptionsLoadCallback   = (cb : OptionsLoaded) => OptionsLoadCallback = cb;
 export const clearOptionsLoadCallback = ()                   => OptionsLoadCallback = _emptyCB;
 
 export const setNFTCollectionsLoadCallback   = (cb : NFTCollectionsLoaded) => NFTCollectionsLoadCallback = cb;
 export const clearNFTCollectionsLoadCallback = ()                          => NFTCollectionsLoadCallback = _emptyCB;
 
+export let AssetsLoadCallback         : AssetsLoaded;
 export let OptionsLoadCallback        : OptionsLoaded;
 export let NFTCollectionsLoadCallback : NFTCollectionsLoaded;
 
 type OptionsLoaded        = () => void;
+type AssetsLoaded         = () => void;
 type NFTCollectionsLoaded = (arr : NFTAsset[]) => void;
 
 const _emptyCB = () => {};
